1f3a4387593bcac107ec33a6a59deae3e0e26040
Apolline Petit <apollinejrpetit@gmail.com>
2024-04-26 16:39:20 +0200

    Removal of an old function and simplification for correcting the counts

--- scripts/analyses/Corrected_counts.R
+++ scripts/analyses/Corrected_counts.R
@@ -3,12 +3,14 @@ source("scripts/functions/RNAseq_analyses.R")
 library(corrplot)
 library(RColorBrewer)
 ################################################################################
+#List of ribosomes from annotation file
+ribosomes <- str_split(subset.pattern(read.csv(file="All_Data/Annotation_r.csv", sep='\t'), "ribosom", 4)[,3], as.character("\\."), n=2, simplify = TRUE)[,1]
+min <-  10 #min = reads mean min for genes to be considered as outliers
+#"Group" or coloumn of ref for ComBat_seq
+group <- 8
+################################################################################
 #ONLY 01
 ################################################################################
-min <-  10 #min = reads mean min to be considered as outliers
-#Threshold
-FCmin <- 2
-pj <- 0.05
 #Data
 quantData <- list.files(path = "All_Data/COUNT/", full.names=TRUE, recursive = TRUE, pattern = "t_data.ctab")
 quantData <- quantData[order(as.character(quantData), method = c("radix"))]
@@ -19,18 +21,22 @@ samp.info <- read.csv("All_Data/samples_design.csv")
 samp.info <- samp.info[order(as.character(samp.info$sample_id), method = c("radix")),]
 samp.info <- subset(samp.info, ancester==01)
 samp.info[,2:ncol(samp.info)] <- lapply(samp.info[,2:ncol(samp.info)] , factor)
-#"Group" or coloumn of ref for ComBat_seq
-group <- 8
-#List of ribosomes from annotation file
-ribosomes <- str_split(subset.pattern(read.csv(file="All_Data/Annotation_r.csv", sep='\t'), "ribosom", 4)[,3], as.character("\\."), n=2, simplify = TRUE)[,1]
-name <- "01_ComBatYearBatch"
 ################################################################################
-dds_all <- count_read_all(quantData=quantData, samp.info=samp.info, min=min, ribosomes.array=ribosomes, filename = name, batcheffect=FALSE)
-counts_raw_corrected <- ComBat_for_batch(counts(estimateSizeFactors(dds_all), normalized=FALSE), samp.info=samp.info, BiorepTemp=TRUE, group=group,
+#Import count data
+tmp <- read_tsv(quantData[1])
+tx2gene <- tmp[, c("t_name", "gene_name")]
+txi1 <- tximport(files=quantData, type = "stringtie", tx2gene = tx2gene, readLength=75) 
+dds <- DESeqDataSetFromTximport(txi = txi1, colData=samp.info, design = ~ 0 + Pop) 
+#Remove low reads
+dds <- dds[rowMeans(counts(dds))>=min,]
+#Remove ribosomes
+dds <- dds[!names(dds)%in%ribosomes,] 
+
+counts_raw_corrected <- ComBat_for_batch(counts(estimateSizeFactors(DESeq(dds)), normalized=FALSE), samp.info=samp.info, BiorepTemp=TRUE, group=group,
                                          T23=c(3,4,7,8,11,12,15,16,19,20,23,24,27,28,31,32,35,36,39,40,42,43),
                                          T17=c(1,2,5,6,9,10,13,14,17,18,21,22,25,26,29,30,33,34,37,38,41))
-dds_all <- DESeq(DESeqDataSetFromMatrix(counts_raw_corrected, colData = samp.info, design = ~ 0 + Pop))
-write.csv(counts(estimateSizeFactors(dds_all), normalized=TRUE), "scripts/data/counts_01_nomalized_corrected.csv")
+dds_corrected <- DESeq(DESeqDataSetFromMatrix(counts_raw_corrected, colData = samp.info, design = ~ 0 + Pop))
+write.csv(counts(estimateSizeFactors(dds_corrected), normalized=TRUE), "scripts/data/counts_01_nomalized_corrected.csv")
 write.csv(counts_raw_corrected, "scripts/data/counts_01_raw_corrected.csv")
 
 ################################################################################
@@ -46,13 +52,20 @@ samp.info <- read.csv("All_Data/samples_design.csv")
 samp.info <- samp.info[order(as.character(samp.info$sample_id), method = c("radix")),]
 samp.info <- subset(samp.info, ancester==44)
 samp.info[,2:ncol(samp.info)] <- lapply(samp.info[,2:ncol(samp.info)] , factor)
-#List of ribosomes from annotation file
-name <- "44_ComBatYearBatch"
 ################################################################################
-dds_all <- count_read_all(quantData=quantData, samp.info=samp.info, min=min, ribosomes.array=ribosomes, filename = name)
-counts_raw_corrected <- ComBat_for_batch(counts(estimateSizeFactors(dds_all), normalized=FALSE), samp.info=samp.info, BiorepTemp=TRUE, group=group,
+#Import count data
+tmp <- read_tsv(quantData[1])
+tx2gene <- tmp[, c("t_name", "gene_name")]
+txi1 <- tximport(files=quantData, type = "stringtie", tx2gene = tx2gene, readLength=75) 
+dds <- DESeqDataSetFromTximport(txi = txi1, colData=samp.info, design = ~ 0 + Pop) 
+#Remove low reads
+dds <- dds[rowMeans(counts(dds))>=min,]
+#Remove ribosomes
+dds <- dds[!names(dds)%in%ribosomes,] 
+
+counts_raw_corrected <- ComBat_for_batch(counts(estimateSizeFactors(DESeq(dds)), normalized=FALSE), samp.info=samp.info, BiorepTemp=TRUE, group=group,
                                          T23=c(3,4,7,8,11,12,15,18,19,21,22,25,26,29,30,31,32,35,36),
                                          T17=c(1,2,5,6,9,10,13,14,16,17,20,23,24,27,28,33,34))
-dds_all <-DESeq(DESeqDataSetFromMatrix(counts_raw_corrected, colData = samp.info, design = ~ 0 + Pop))
-write.csv(counts(estimateSizeFactors(dds_all), normalized=TRUE), "scripts/data/counts_44_nomalized_corrected.csv")
+dds_corrected <-DESeq(DESeqDataSetFromMatrix(counts_raw_corrected, colData = samp.info, design = ~ 0 + Pop))
+write.csv(counts(estimateSizeFactors(dds_corrected), normalized=TRUE), "scripts/data/counts_44_nomalized_corrected.csv")
 write.csv(counts_raw_corrected, "scripts/data/counts_44_raw_corrected.csv")
--- scripts/functions/RNAseq_analyses.R
+++ scripts/functions/RNAseq_analyses.R
@@ -5,47 +5,6 @@ library(tximportData)
 library(stringr)
 library(ggplot2)
 ################################################################################
-
-count_read_all <- function(quantData, samp.info, min=10, ribosomes.array=ribosomes, filename="all", readLength=75, batcheffect=FALSE){
-  #Function to run a DESeq2 analyses on raw read counts.
-  #readLength =-s argument of stringtie, default is 75
-  tmp <- read_tsv(quantData[1])
-  tx2gene <- tmp[, c("t_name", "gene_name")]
-  txi1 <- tximport(files=quantData, type = "stringtie", tx2gene = tx2gene, readLength=readLength) 
-  tx.count1 <- as.data.frame(txi1[["counts"]])
-  
-  
-  if(identical(samp.info$sample_id, names(quantData))==TRUE){
-    samp.info[,2:ncol(samp.info)] <- lapply(samp.info[,2:ncol(samp.info)] , factor)
-    #Tximport data with : DeSEQ2
-    if(batcheffect) reads.raw.count <- DESeqDataSetFromTximport(txi = txi1, colData=samp.info,
-                                                                   design =  ~ 1 + Year + Year:Batch2 + Year:Evol) #batch effect accounted for ~ Batch + Evol
-    else reads.raw.count <- DESeqDataSetFromTximport(txi = txi1, colData=samp.info,
-                                                design = ~ 0 + Pop) #Contrast null
-    #Remove genes with a read mean inferior at x
-    reads.raw.count <- reads.raw.count[rowMeans(counts(reads.raw.count))>=min,]
-    # #Remove outliers
-    #reads.raw.count <- reads.raw.count[rowSums(counts(reads.raw.count))<=max,]
-    #Remove ribosomes
-    reads.raw.count <- reads.raw.count[!names(reads.raw.count)%in%ribosomes,] 
-    
-    write.csv(counts(reads.raw.count), paste0("scripts/data/",filename,"_raw_read_counts.csv"))
-    
-    # reads.raw.count2 <- estimateSizeFactors(reads.raw.count)
-    # sizeFactors(reads.raw.count2)
-    reads.normalized.counts <- counts(estimateSizeFactors(reads.raw.count), normalized=TRUE)
-    #Filter with edgeR to improve normalization
-    m_design <- model.matrix(~ 1 + Year, samp.info)
-    print(length(which(edgeR::filterByExpr(reads.normalized.counts, design=m_design)==FALSE)))
-    reads.normalized.counts <- reads.normalized.counts[edgeR::filterByExpr(reads.normalized.counts, design=m_design),]
-    write.csv(reads.normalized.counts, file=paste0("scripts/data/",filename,"_normalized_counts.csv"))
-
-    dds <- DESeq(reads.raw.count)
-
-    return(dds)
-  }else(print("Sample_info and sample files are not in the same order"))
-}
-
 #Function to subset by string pattern
 subset.pattern <- function(data, patterns, var, keep.found = TRUE, useBytes = TRUE){
   data$y <- grepl(pattern = paste0(patterns, collapse="|"), x = data[, var],
@@ -87,7 +46,7 @@ plotPCA.custom <- function (object, intgroup = "condition", ntop = 500, returnDa
 
 
 
-ComBat_for_batch <- function(counts_raw, BiorepTemp=FALSE, samp.info, group=8, T23=c(3,4,7,8,11,12,15,16,19,20,23,24,27,28,31,32,35,36,39,40,42,43), 
+ComBat_for_batch <- function(counts_raw, BiorepTemp=TRUE, samp.info, group=8, T23=c(3,4,7,8,11,12,15,16,19,20,23,24,27,28,31,32,35,36,39,40,42,43), 
                              T17=c(1,2,5,6,9,10,13,14,17,18,21,22,25,26,29,30,33,34,37,38,41)){
   #group=samp.info$Evol initially but can be changed
   samp.info[,2:ncol(samp.info)] <- lapply(samp.info[,2:ncol(samp.info)] , factor)
